6598cd9ddf2047a4ae1e3a1f5ef6849e6f5925c1
Felipe Ferrarini <ferrarinifelipe@gmail.com>
2021-01-26 23:38:53 -0300

    fix: QuizBackground import error

--- src/components/QuizBackground/index.ts
+++ src/components/QuizBackground/index.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { ThemeProps } from '../../../pages/_app';
+import { ThemeProps } from '../../pages/_app';
 
 interface Props {
   backgroundImage: string;
